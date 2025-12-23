90123a49a0eae000b166c73150ef832c1eaaf8d7
Julian Bourne <julian@britesc.com>
2025-12-23 20:17:09 +0100

    second commit

---
+++ include/WS35TLCD_PINS.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef __WS35TLCD_PINS_H__
+#define __WS35TLCD_PINS_H__
+
+// Waveshare 3.5" ESP32-S3 Display Pin Definitions (Arduino GFX)
+
+// TFT Display Pins (SPI)
+#define GFX_BL     6     // Backlight
+#define SPI_MISO   2
+#define SPI_MOSI   1
+#define SPI_SCLK   5
+#define LCD_CS    -1     // Chip Select (not used)
+#define LCD_DC     3     // Data/Command
+#define LCD_RST   -1     // Reset (not used)
+
+// SD Card Pins (SPI)
+#define SD_CS      46
+#define SD_MOSI    7
+#define SD_MISO    8
+#define SD_SCK     6
+
+// Touch Screen Pins (I2C) - if needed later
+#define TOUCH_SDA  38
+#define TOUCH_SCL  39
+#define TOUCH_INT  18
+#define TOUCH_RST  -1
+
+// Display Specifications
+#define TFT_WIDTH  320
+#define TFT_HEIGHT 480
+
+#endif // __WS35TLCD_PINS_H__
